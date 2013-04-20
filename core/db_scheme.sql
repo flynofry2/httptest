29f11f28d141b7e68c86ec9e00a2b10154c5ecc6
lihongfu <tony.li.fly@gmail.com>
2013-04-21 00:00:04 +0800

    init

---
+++ core/db_scheme.sql
@@ -0,0 +1,19 @@
+CREATE TABLE t_category (
+  id          INT(11)      NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  parent_id   INT          NOT NULL,
+  path        VARCHAR(100) NOT NULL,
+  name        VARCHAR(100) NOT NULL,
+  description VARCHAR(100)
+);
+
+
+CREATE TABLE t_content (
+  id          INT(11)      NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title       VARCHAR(100) NOT NULL,
+  url         VARCHAR(150) NOT NULL,
+  params      VARCHAR(1000),
+  detail      TEXT,
+  category_id INT(11),
+  insert_date DATETIME     NOT NULL,
+  update_date DATETIME     NOT NULL
+);
---
+++ core/settings.py
@@ -0,0 +1,15 @@
+#web template path
+TEMPLATE_FOLDER = 'templates/'
+
+#database config
+DB_TYPE = 'mysql'
+DB_HOST = 'localhost'
+DB_NAME = 'ccgk'
+DB_USER = 'root'
+DB_PWD = 'admin'
+
+#upload file path
+UPLOAD_FILE_PATH = 'E:/projects/ccgk/src/static/upload'
+
+#pagination
+SIZE_OF_PAGE = 20
